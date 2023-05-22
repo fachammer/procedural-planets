3f3a59554e66b2d2ff95cfb2ee4eab6e6295f2b7
Fabian Achammer <fabian@achammer.dev>
2023-05-22 19:32:12 +0200

    remove unnecessary setting of texture parameters

--- src/ProceduralPlanets.cpp
+++ src/ProceduralPlanets.cpp
@@ -100,11 +100,6 @@ public:
             SOIL_LOAD_AUTO,
             SOIL_CREATE_NEW_ID,
             SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS);
-        glBindTexture(GL_TEXTURE_2D, textureId);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     }
     Texture(const Texture &) = delete;
     Texture &operator=(const Texture &) = delete;
