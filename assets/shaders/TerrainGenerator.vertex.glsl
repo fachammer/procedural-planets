2b4210e38890f2759cdd7c84c269952740e8aa47
Fabian Achammer <fabian@achammer.dev>
2023-05-26 14:47:32 +0200

    use psrdnoise instead of simplex noise

--- assets/shaders/TerrainGenerator.vertex.glsl
+++ assets/shaders/TerrainGenerator.vertex.glsl
@@ -15,97 +15,83 @@ uniform float maxNegativeHeight;
 uniform float maxPositiveHeight;
 uniform vec3 noiseOffset;
 
-//  Simplex 3D Noise
-//  by Ian McEwan, Ashima Arts
-vec4 permute(vec4 x) {
-    return mod(((x * 34.0) + 1.0) * x, 289.0);
-}
-vec4 taylorInvSqrt(vec4 r) {
-    return 1.79284291400159 - 0.85373472095314 * r;
-}
+// psrdnoise (c) Stefan Gustavson and Ian McEwan,
+// ver. 2021-12-02, published under the MIT license:
+// https://github.com/stegu/psrdnoise/
 
-float snoise(vec3 v, out vec3 gradient) {
-    const vec2 C = vec2(1.0 / 6.0, 1.0 / 3.0);
-    const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
-
-    v += noiseOffset;
-
-    // First corner
-    vec3 i = floor(v + dot(v, C.yyy));
-    vec3 x0 = v - i + dot(i, C.xxx);
-
-    // Other corners
-    vec3 g = step(x0.yzx, x0.xyz);
-    vec3 l = 1.0 - g;
-    vec3 i1 = min(g.xyz, l.zxy);
-    vec3 i2 = max(g.xyz, l.zxy);
-
-    //  x0 = x0 - 0. + 0.0 * C
-    vec3 x1 = x0 - i1 + 1.0 * C.xxx;
-    vec3 x2 = x0 - i2 + 2.0 * C.xxx;
-    vec3 x3 = x0 - 1. + 3.0 * C.xxx;
-
-    // Permutations
-    i = mod(i, 289.0);
-    vec4 p = permute(permute(permute(i.z + vec4(0.0, i1.z, i2.z, 1.0)) + i.y + vec4(0.0, i1.y, i2.y, 1.0)) + i.x + vec4(0.0, i1.x, i2.x, 1.0));
-
-    // Gradients
-    // ( N*N points uniformly over a square, mapped onto an octahedron.)
-    float n_ = 1.0 / 7.0; // N=7
-    vec3 ns = n_ * D.wyz - D.xzx;
-
-    vec4 j = p - 49.0 * floor(p * ns.z * ns.z);  //  mod(p,N*N)
-
-    vec4 x_ = floor(j * ns.z);
-    vec4 y_ = floor(j - 7.0 * x_);    // mod(j,N)
-
-    vec4 x = x_ * ns.x + ns.yyyy;
-    vec4 y = y_ * ns.x + ns.yyyy;
-    vec4 h = 1.0 - abs(x) - abs(y);
-
-    vec4 b0 = vec4(x.xy, y.xy);
-    vec4 b1 = vec4(x.zw, y.zw);
-
-    vec4 s0 = floor(b0) * 2.0 + 1.0;
-    vec4 s1 = floor(b1) * 2.0 + 1.0;
-    vec4 sh = -step(h, vec4(0.0));
-
-    vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
-    vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
-
-    vec3 p0 = vec3(a0.xy, h.x);
-    vec3 p1 = vec3(a0.zw, h.y);
-    vec3 p2 = vec3(a1.xy, h.z);
-    vec3 p3 = vec3(a1.zw, h.w);
-
-    //Normalise gradients
-    vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
-    p0 *= norm.x;
-    p1 *= norm.y;
-    p2 *= norm.z;
-    p3 *= norm.w;
-
-    // Mix final noise value
-    vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
-    vec4 m2 = m * m;
-    vec4 m4 = m2 * m2;
-
-    vec4 pdotx = vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3));
-
-    vec4 temp = m2 * m * pdotx;
-    gradient = -8.0 * (temp.x * x0 + temp.y * x1 + temp.z * x2 + temp.w * x3);
-    gradient += m4.x * p0 + m4.y * p1 + m4.z * p2 + m4.w * p3;
-    gradient *= 42.0;
-
-    return 42.0 * dot(m4, pdotx);
+vec4 permute(vec4 i) {
+    vec4 im = mod(i, 289.0);
+    return mod(((im * 34.0) + 10.0) * im, 289.0);
 }
 
+float psrdnoise(vec3 x, vec3 period, float alpha, out vec3 gradient) {
+    const mat3 M = mat3(0.0, 1.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 0.0);
+    const mat3 Mi = mat3(-0.5, 0.5, 0.5, 0.5, -0.5, 0.5, 0.5, 0.5, -0.5);
+    vec3 uvw = M * x;
+    vec3 i0 = floor(uvw), f0 = fract(uvw);
+    vec3 g_ = step(f0.xyx, f0.yzz), l_ = 1.0 - g_;
+    vec3 g = vec3(l_.z, g_.xy), l = vec3(l_.xy, g_.z);
+    vec3 o1 = min(g, l), o2 = max(g, l);
+    vec3 i1 = i0 + o1, i2 = i0 + o2, i3 = i0 + vec3(1.0);
+    vec3 v0 = Mi * i0, v1 = Mi * i1, v2 = Mi * i2, v3 = Mi * i3;
+    vec3 x0 = x - v0, x1 = x - v1, x2 = x - v2, x3 = x - v3;
+    if(any(greaterThan(period, vec3(0.0)))) {
+        vec4 vx = vec4(v0.x, v1.x, v2.x, v3.x);
+        vec4 vy = vec4(v0.y, v1.y, v2.y, v3.y);
+        vec4 vz = vec4(v0.z, v1.z, v2.z, v3.z);
+        if(period.x > 0.0)
+            vx = mod(vx, period.x);
+        if(period.y > 0.0)
+            vy = mod(vy, period.y);
+        if(period.z > 0.0)
+            vz = mod(vz, period.z);
+        i0 = floor(M * vec3(vx.x, vy.x, vz.x) + 0.5);
+        i1 = floor(M * vec3(vx.y, vy.y, vz.y) + 0.5);
+        i2 = floor(M * vec3(vx.z, vy.z, vz.z) + 0.5);
+        i3 = floor(M * vec3(vx.w, vy.w, vz.w) + 0.5);
+    }
+    vec4 hash = permute(permute(permute(vec4(i0.z, i1.z, i2.z, i3.z)) + vec4(i0.y, i1.y, i2.y, i3.y)) + vec4(i0.x, i1.x, i2.x, i3.x));
+    vec4 theta = hash * 3.883222077;
+    vec4 sz = hash * -0.006920415 + 0.996539792;
+    vec4 psi = hash * 0.108705628;
+    vec4 Ct = cos(theta), St = sin(theta);
+    vec4 sz_prime = sqrt(1.0 - sz * sz);
+    vec4 gx, gy, gz;
+    if(alpha != 0.0) {
+        vec4 px = Ct * sz_prime, py = St * sz_prime, pz = sz;
+        vec4 Sp = sin(psi), Cp = cos(psi), Ctp = St * Sp - Ct * Cp;
+        vec4 qx = mix(Ctp * St, Sp, sz), qy = mix(-Ctp * Ct, Cp, sz);
+        vec4 qz = -(py * Cp + px * Sp);
+        vec4 Sa = vec4(sin(alpha)), Ca = vec4(cos(alpha));
+        gx = Ca * px + Sa * qx;
+        gy = Ca * py + Sa * qy;
+        gz = Ca * pz + Sa * qz;
+    } else {
+        gx = Ct * sz_prime;
+        gy = St * sz_prime;
+        gz = sz;
+    }
+    vec3 g0 = vec3(gx.x, gy.x, gz.x), g1 = vec3(gx.y, gy.y, gz.y);
+    vec3 g2 = vec3(gx.z, gy.z, gz.z), g3 = vec3(gx.w, gy.w, gz.w);
+    vec4 w = 0.5 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3));
+    w = max(w, 0.0);
+    vec4 w2 = w * w, w3 = w2 * w;
+    vec4 gdotx = vec4(dot(g0, x0), dot(g1, x1), dot(g2, x2), dot(g3, x3));
+    float n = dot(w3, gdotx);
+    vec4 dw = -6.0 * w2 * gdotx;
+    vec3 dn0 = w3.x * g0 + dw.x * x0;
+    vec3 dn1 = w3.y * g1 + dw.y * x1;
+    vec3 dn2 = w3.z * g2 + dw.z * x2;
+    vec3 dn3 = w3.w * g3 + dw.w * x3;
+    gradient = 39.5 * (dn0 + dn1 + dn2 + dn3);
+    return 39.5 * n;
+}
 float map(float value, float inMin, float inMax, float outMin, float outMax) {
     return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
 float noise(vec3 position, out vec3 gradient) {
-    return snoise(position, gradient);
+    return psdrnoise(position, 1, 1, gradient);
 }
 
 float smax(float a, float b, float k, out float h) {
@@ -118,7 +104,6 @@ float smax(float a, float b, float k, out float h) {
 float[] amplitudes = float[](1, 4, 4, 3, 1, 1, 0.5);
 float[] frequencies = float[](4 / 1000.0, 8 / 1000.0, 16 / 1000.0, 32 / 1000.0, 64 / 1000.0, 128 / 1000.0, 256 / 1000.0);
 float elevation(vec3 position, float minElevation, float maxElevation, out vec3 gradient) {
-
     float totalElevation = 0;
     gradient = vec3(0, 0, 0);
     float totalAmplitude = 0;
